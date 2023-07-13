9588a17e833f4174f00bd1d6989d73cb1b76f489
Madhava Varma <madhavavarma.m@gmail.com>
2023-07-13 09:54:32 +0530

    Adding MUI Core - UI Library

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client';
+import Button from '@mui/material/Button';
 
 var root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <div>
-        Welcome from index.tsx
+         <Button variant="contained">Hello World</Button>
     </div>
 )
